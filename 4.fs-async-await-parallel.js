635047388217147079e3905f87a0bc923bab885d
Ricardo Ferreira <ricardoferreiran@gmail.com>
2026-01-08 00:03:16 +0100

    segundo commit

---
+++ 4.fs-async-await-parallel.js
@@ -0,0 +1,14 @@
+// Lee varios archivos en paralelo usando Promise.all.
+// Se ejecuta el .then() cuando todos los archivos han sido leídos correctamente.
+
+const { readFile } = require('node:fs/promises')
+
+Promise.all([
+  readFile('./archivo.txt', 'utf-8'),
+  readFile('./archivo2.txt', 'utf-8'),
+  readFile('./archivo3.txt', 'utf-8'),
+]).then(([text, secondText, thirdText]) => {
+  console.log('Este es el primer comentario: ', text)
+  console.log('Este es el segundo comentario: ', secondText)
+  console.log('Este es el tercer comentario: ', thirdText)
+})
