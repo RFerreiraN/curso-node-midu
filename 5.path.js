c43d0a0cdf5954c0cc33a438189fbc4817e20500
Ricardo Ferreira <ricardoferreiran@gmail.com>
2026-01-09 09:18:48 +0100

    asincronia y path

--- 5.path.js
+++ 5.path.js
@@ -1,2 +1,12 @@
-const path = require('node:path');
+const path = require('node:path')
 
+console.log(path.sep) // => Nos permite saber como son la separación con barras en las rutas en nuestro Sistema Operativo SO
+
+const filePath = path.join('content', 'subfolder', 'test.txt') // => Nos permite unir rutas de archivos o carpetas de una manera más sencilla y compatible con el SO
+console.log(filePath)
+
+const fileName = path.basename('/Users/mauriciopoppe/Projects/node/content/subfolder/test.txt') // => Nos permite obtener el nombre del archivo o carpeta de una ruta completa
+console.log(fileName);
+
+const extension = path.extname('/Users/mauriciopoppe/Projects/node/content/subfolder/test.txt') // => Nos permite obtener la extension del archivo (.txt o .jpg, etc)
+console.log(extension)
