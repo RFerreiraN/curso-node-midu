c865d43fe448d678b46c9a4dc1b8275a3687a4dc
Ricardo Ferreira <ricardoferreiran@gmail.com>
2026-01-10 00:47:49 +0100

    process

---
+++ 6.ls.js
@@ -0,0 +1,47 @@
+const fs = require('node:fs')
+const path = require('node:path')
+
+// readdir lee el contenido del directorio actual (.) y ejecuta un callback.
+// El callback recibe un error (si ocurre) y un array de strings con los nombres de los archivos.
+
+fs.readdir('.', (err, files) => { 
+  if (err) {
+    console.error('Ha ocurrido un error: ', err) // manejo de errores
+    return
+  }
+
+  // Usamos map para transformar el array de nombres de archivos
+  // en un nuevo array que contiene solo sus extensiones
+  const extension = files.map(file => { 
+    return path.extname(file)
+  })
+
+  const arrayJS = []
+  const arrayMJS = []
+  const arrayTXT = []
+
+ // Recorremos el array de extensiones y clasificamos cada una
+ // usando un switch para guardarlas en distintos arrays
+  extension.forEach((ext, index) => {
+    switch (ext) {
+      case '.js':
+        arrayJS.push(ext,index)
+        break;
+      case '.mjs':
+        arrayMJS.push(ext,index)
+        break;
+      case '.txt':
+        arrayTXT.push(ext,index)
+        break;
+
+      default:
+        break;
+    }
+
+  })
+
+  console.log(arrayJS)
+  console.log(arrayMJS)
+  console.log(arrayTXT)
+
+});
---
+++ 7.process.js
@@ -0,0 +1,18 @@
+//Argumentos de entrada
+//console.log(process.argv)
+
+// Controlar el proceso de salida ( si se la pasa parametro 0 es que todo fue correcto, si pasamos parametro 1 hubo problemas y decidimos salir del proceso)
+// process.exit(0)
+
+// podemos escuchar los eventos del proceso
+
+// process.on('exit', () => {
+//   console.log('Has salido')
+// })
+
+// current working directory
+console.log(process.cwd())
+
+//platform
+
+console.log(process.platform())
