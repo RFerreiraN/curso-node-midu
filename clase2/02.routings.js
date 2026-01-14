a8193a77d82df1b57000ed8ad96f11d9468cb6c2
RFerreiraN <ricardevferreira@gmail.com>
2026-01-14 22:40:46 +0100

    practica de rutas con nodejs nativo

--- clase2/02.routings.js
+++ clase2/02.routings.js
@@ -61,28 +61,46 @@
 // })
 
 const http = require('node:http')
-
-const port = process.env.PORT ?? 1234
+const fs = require('node:fs')
 const dittoJSON = require('../pokemon/ditto.json')
 
-const processRequest = (req, res) => {
-  const { url, method } = req
+const port = process.env.PORT ?? 1234
 
+const processRequest = (req, res) => {
+  const { method, url } = req
   switch (method) {
     case 'GET':
       switch (url) {
         case '/':
           res.setHeader('Content-type', 'text/html; charset=utf-8')
-          res.end('<h1>Pagína de início</h1>')
+          res.end('<h1>Página de início</h1>')
           break
         case '/pokemon/ditto':
           res.setHeader('Content-type', 'application/json; charset=utf-8')
           res.end(JSON.stringify(dittoJSON))
           break
+        case '/imagen':
+          fs.readFile('../placa.png', (err, data) => {
+            if (err) {
+              if (err.code === 'ENOENT') {
+                res.statusCode = 404
+                res.setHeader('Content-type', 'text/html; charset=utf-8')
+                res.end('<h1>404 Not Found</h1>')
+              } else {
+                res.statusCode = 500
+                res.setHeader('Content-type', 'text/html; charset=utf-8')
+                res.end('<h1>500 Internal Server Error</h1>')
+              }
+            }
+            res.statusCode = 200
+            res.setHeader('Content-type', 'image/png')
+            res.end(data)
+          })
+          break
         default:
           res.statusCode = 404
-          res.setHeader('Content-type', 'text/html')
-          res.end('<h1>404 Not Founded</h1>')
+          res.setHeader('Content-type', 'text/html; charset=utf-8')
+          res.end('<h1>404 Not Found</h1>')
           break
       }
       break
@@ -93,21 +111,25 @@ const processRequest = (req, res) => {
           req.on('data', chunk => {
             body += chunk.toString()
           })
-
           req.on('end', () => {
             const data = JSON.parse(body)
-            res.writeHead('201', { 'Content-Type': 'application/json: charset=utf-8' })
+            res.writeHead('201', { 'Content-type': 'application/json' })
             res.end(JSON.stringify(data))
           })
         }
-
           break
         default:
-          res.statusCode = 404
-          res.setHeader('Content-type', 'text/html')
-          res.end('<h1>404 Not Founded</h1>')
+          res.statusCode = 500
+          res.setHeader('Content-type', 'text/html; charset=utf-8')
+          res.end('<h1>500 Internal Server Error</h1>')
           break
       }
+      break
+    default:
+      res.statusCode = 404
+      res.setHeader('Content-type', 'text/html; charset=utf-8')
+      res.end('<h1>404 Not Found</h1>')
+      break
   }
 }
 
